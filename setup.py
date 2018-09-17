30ad76ae07413f27d201d13c5309711e1f098b92
Ignacio De La Madrid <ignacio.delamadrid@gmail.com>
2018-09-17 14:32:08 -0500

    Core package folder and setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='tellmethetruth',
+      version='0.0',
+      description='Evaluation Tree for boolean values or Python objects',
+      url='https://github.com/inakidelamadrid/tellmethetruth',
+      author='Ignacio De La Madrid',
+      author_email='ignacio.delamadrid@gmail.com',
+      license='MIT',
+      packages=['tellmethetruth'],
+      zip_safe=False)
