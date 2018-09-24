6d6ebe90133cff1b6ead7fc0e338e327e918c117
Ignacio De La Madrid <ignacio.delamadrid@gmail.com>
2018-09-24 16:29:37 -0500

    Add util functions to ease the creation of trees/subtrees

--- tellmethetruth/utils.py
+++ tellmethetruth/utils.py
@@ -1,31 +1,14 @@
-from collections import deque
+from .tree import BinaryTree
+from .logical import LogicalNode
 
 
-def print_tree(root):
-    buf = deque()
-    output = []
-    if not root:
-        print("$")
-    else:
-        buf.append(root)
-        count, nextCount = 1, 0
-        while count:
-            node = buf.popleft()
-            if node:
-                output.append(node.value)
-                count -= 1
-                for n in (node.left, node.right):
-                    if n:
-                        buf.append(n)
-                        nextCount += 1
-                    else:
-                        buf.append(None)
-            else:
-                output.append("$")
-            if not count:
-                print(output)
-                output = []
-                count, nextCount = nextCount, 0
-        # print the remaining all empty leaf node part
-        output.extend(["$"] * len(buf))
-        print(output)
+def build_tree(left, right, op):
+    return BinaryTree(left, right, op)
+
+
+def logical_or_tree(left, right):
+    return build_tree(left, right, LogicalNode.OR)
+
+
+def logical_and_tree(left, right):
+    return build_tree(left, right, LogicalNode.AND)
